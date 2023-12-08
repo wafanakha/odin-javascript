bf66a02f1691c3f626539f0d8a0363cd1c1c45cb
wafanakha <wafanakha15@gmail.com>
2023-12-08 21:42:09 +0700

    adding object article and set the prototype to book

--- constructor.js
+++ constructor.js
@@ -11,14 +11,19 @@ function Book(title, author, pages, read) {
       }`
     );
 }
-
+function Article(title) {
+  this.title = title;
+}
+Object.setPrototypeOf(Article.prototype, Book.prototype);
 const lily = new Book("Mario the idea", "Tonaka Matsunaga", 300, false);
 lily.info();
 
-console.log(Object.getPrototypeOf(lily) === Book.prototype);
-
+const caveStory = new Article("The Cave Story");
+caveStory.author = "James Miller";
+caveStory.pages = 140;
+caveStory.read = true;
 Book.prototype.infoAuthor = function () {
   console.log("This book written by " + this.author);
 };
-
+caveStory.infoAuthor();
 lily.infoAuthor();
