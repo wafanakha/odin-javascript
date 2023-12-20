d15d111f7cad8705a1a39f8db26e97436eeebfe4
wafanakha <wafanakha15@gmail.com>
2023-12-20 20:37:47 +0700

    adding basic factory functoin

--- shorthand.js
+++ shorthand.js
@@ -2,3 +2,9 @@ const User = function (name) {
   this.name = name;
   this.nick = `@ ${this.name}`;
 };
+
+// this is same
+function createUser(name) {
+  const nick = "@" + name;
+  return { name, nick };
+}
